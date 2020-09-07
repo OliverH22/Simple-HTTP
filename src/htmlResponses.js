04f8dbd06b74406df0a9a81b52594701122e0671
OliverH22 <42745916+OliverH22@users.noreply.github.com>
2020-09-07 12:11:18 -0400

    Add files via upload

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -3,5 +3,18 @@ const fs = require('fs');
 const index = fs.readFilesync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFilesync(`${__dirname}/../client/client2.html`);
 
+const getIndex = (request, response) => {
+    
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write(index);
+    response.end();
+};
+
+const getPage2 = (request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(page2);
+    response.end();
+};
+
 module.exports.getIndex = getIndex;
 module.exports.getPage2 = getPage2;
